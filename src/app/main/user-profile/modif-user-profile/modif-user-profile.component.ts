38d1fb38f98f6ffe7c2aec712de37678d04aea32
raymond <rerezheng@gmail.com>
2025-01-28 12:21:21 +0100

    rename logIn to login, create createPaper, modifUserProfile

---
+++ src/app/main/user-profile/modif-user-profile/modif-user-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modif-user-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './modif-user-profile.component.html',
+  styleUrl: './modif-user-profile.component.scss'
+})
+export class ModifUserProfileComponent {
+
+}
