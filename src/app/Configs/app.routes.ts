375a7dc1fcc358e7c1e87e9020743c3ceccc08c9
raymond <rerezheng@gmail.com>
2025-01-28 11:08:21 +0100

    add register, login component

--- src/app/Configs/app.routes.ts
+++ src/app/Configs/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import {TestComponent} from "../test/test.component";
 import {AccueilComponent} from "../main/accueil/accueil.component";
+import {UserProfileComponent} from "../main/user-profile/user-profile.component";
+import {RegisterComponent} from "../main/register/register.component";
+import {LogInComponent} from "../main/log-in/log-in.component";
 
 export const routes: Routes = [
 
   { path: 'test', component: TestComponent },
   { path: 'accueil', component: AccueilComponent },
+  { path: 'profile/:id', component: UserProfileComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'logIn', component: LogInComponent},
   { path: '', redirectTo: '/accueil', pathMatch: 'full' },
   { path: '**', component: AccueilComponent },
 
