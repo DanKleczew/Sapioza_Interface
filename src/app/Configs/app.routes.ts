940a57133541eb2a95ee685bc570e4b5294cc27d
raymond <rerezheng@gmail.com>
2025-01-28 12:12:21 +0100

    update routes, add search, createPaper, userFollowersData,

--- src/app/Configs/app.routes.ts
+++ src/app/Configs/app.routes.ts
@@ -3,6 +3,9 @@ import {TestComponent} from "../test/test.component";
 import {AccueilComponent} from "../main/accueil/accueil.component";
 import {RegisterComponent} from "../main/register/register.component";
 import {LogInComponent} from "../main/log-in/log-in.component";
+import {SearchComponent} from "../main/search/search.component";
+import {UserProfileComponent} from "../main/user-profile/user-profile.component";
+import {CreatePaperComponent} from "../main/create-paper/create-paper.component";
 
 export const routes: Routes = [
 
@@ -10,6 +13,9 @@ export const routes: Routes = [
   { path: 'accueil', component: AccueilComponent },
   { path: 'register', component: RegisterComponent},
   { path: 'logIn', component: LogInComponent},
+  { path: 'search', component: SearchComponent},
+  { path: 'profile', component: UserProfileComponent},
+  { path: 'createPaper', component: CreatePaperComponent},
   { path: '', redirectTo: '/accueil', pathMatch: 'full' },
   { path: '**', component: AccueilComponent },
 
