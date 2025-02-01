f9044cbf43a15efea7cd9c3146de605e42a16c61
Dan Kleczewski <Dan.Kleczewski1@etu.univ-paris1.fr>
2025-02-01 21:47:50 +0100

    Added loading component

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,11 +4,12 @@ import { PaperQueryService } from '../Services/paper-query.service';
 import { ButtonComponent } from "../main/widgets/buttons/button/button.component";
 import { RichButtonComponent } from "../main/widgets/buttons/rich-button/rich-button.component";
 import { IconButtonComponent } from "../main/widgets/buttons/icon-button/icon-button.component";
+import {LoadingComponent} from "../main/widgets/loading/loading.component";
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [PaperInfoComponent, ButtonComponent, RichButtonComponent, IconButtonComponent],
+  imports: [PaperInfoComponent, ButtonComponent, RichButtonComponent, IconButtonComponent, LoadingComponent],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
