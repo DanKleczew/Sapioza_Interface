3e4b90489ca7c662f013f7e1ba0e9a4eb1a92ae3
Dan Kleczewski <Dan.Kleczewski1@etu.univ-paris1.fr>
2025-02-02 20:35:32 +0100

    Updated some widgets

--- src/app/main/widgets/buttons/rich-button/rich-button.component.ts
+++ src/app/main/widgets/buttons/rich-button/rich-button.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {NgTemplateOutlet} from "@angular/common";
 
 @Component({
   selector: 'app-rich-button',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgTemplateOutlet],
   templateUrl: './rich-button.component.html',
   styleUrl: '../button.component.scss'
 })
